059c8d6ee8fd90c52f13695daf5fc19c831f7af7
quedacoder <quedacoder@gmail.com>
2022-03-25 16:43:53 -0500

    my version of the solution

--- src/com/quedacoder/Main.java
+++ src/com/quedacoder/Main.java
@@ -1,8 +1,37 @@
 package com.quedacoder;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static String USER_PROMPT_TEXT = "Enter number #%d: ";
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("This program will ask for 10 Integer values and sum them together!!");
+        int validUserInput = 1;
+        int textCounter = 1;
+        int sumOfValuesEntered = 0;
+
+        do {
+
+            System.out.println(String.format(USER_PROMPT_TEXT, textCounter));
+            boolean isValidNumber = scanner.hasNextInt();
+
+            if (isValidNumber) {
+                validUserInput++;
+                textCounter++;
+                int number = scanner.nextInt();
+                sumOfValuesEntered += number;
+            }
+            scanner.nextLine();
+        }
+        while (validUserInput <= 10);
+
+        System.out.println("sum = " + sumOfValuesEntered);
+
+        scanner.close();
     }
 }
